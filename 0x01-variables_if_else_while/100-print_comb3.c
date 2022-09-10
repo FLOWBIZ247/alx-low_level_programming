a182bc740e1c1d359829755851b052f146cb9e9a
FLOWBIZ247 <moyosoreoluwaisrael.ajayi@gmail.com>
2022-09-10 07:32:06 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -24,8 +24,13 @@ int main(void)
 
 		if (k < j)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
 		}
 	}
 
